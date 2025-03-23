6f48312e3b9543b556377db9fca6c44313a058d4
moonfeather <moonbite233@gmail.com>
2025-03-24 03:07:51 +0800

    add unit test file

--- example/cExample.c
+++ example/cExample.c
@@ -1,9 +1,10 @@
 #include "packets/CustomControllerPacket.h"
 #include "serialize.h"
 uint8_t buffer[50] = {};
-CustomControlPacket packet = {.cmd = 0x0302};
+CustomControlPacket packet;
 
 int main() {
+    init_custom_control_packet(&packet);
     serialize(&packet, buffer, 50);
     return 0;
 }
