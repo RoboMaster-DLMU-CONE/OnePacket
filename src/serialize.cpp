02f89005cba1ab5876b7a758dcb3aff20cf05202
moonfeather <moonbite233@gmail.com>
2025-03-24 10:36:38 +0800

    don't use gitee go, it's shit

--- src/serialize.cpp
+++ src/serialize.cpp
@@ -4,7 +4,8 @@
 
 int serialize(const void *struct_ptr, uint8_t *buffer, const size_t buffer_len) {
     const auto *base = static_cast<const PacketBase *>(struct_ptr);
-    if (const auto func = S_functionMap[base->cmd]; func != nullptr) {
+    const auto func = S_functionMap[base->cmd];
+    if (func != nullptr) {
         return func(struct_ptr, buffer, buffer_len);
     }
     return 0;
