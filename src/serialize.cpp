b17420e5269317902c0482f126cb05ff5e21b777
moonfeather <moonbite233@gmail.com>
2025-03-24 11:13:50 +0800

    use unordered_map.at

--- src/serialize.cpp
+++ src/serialize.cpp
@@ -1,12 +1,14 @@
+#include <stdexcept>
 #include "serialize.h"
 #include "PacketBase.hpp"
 #include "CustomFunction/CustomFunction.hpp"
 
 int serialize(const void *struct_ptr, uint8_t *buffer, const size_t buffer_len) {
     const auto *base = static_cast<const PacketBase *>(struct_ptr);
-    const auto func = S_functionMap[base->cmd];
-    if (func != nullptr) {
+    try {
+        const auto func = S_functionMap[base->cmd];
         return func(struct_ptr, buffer, buffer_len);
+    } catch (const std::out_of_range &) {
     }
     return 0;
 }
