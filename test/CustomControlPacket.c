3459eda91898645872a3ae50587d788fae8d770a
moonfeather <moonbite233@gmail.com>
2025-03-25 19:30:08 +0800

    pass deserialize test

--- test/CustomControlPacket.c
+++ test/CustomControlPacket.c
@@ -1,8 +1,7 @@
 #include "serialize.h"
+#include "deserialize.h"
 #include "packets/CustomControllerPacket.h"
 #include <assert.h>
-#include <stdbool.h>
-
 CustomControlPacket packet;
 
 void setData() {
@@ -11,9 +10,21 @@ void setData() {
     }
 }
 
+void testHeader(const uint8_t *buffer) {
+    assert(buffer[0] == 0xA5 && buffer[1] == 39 && buffer[2] == 0 && "Wrong serialized frame header.");
+    assert(buffer[5] == 0x02 && buffer[6] == 0x03 && "Wrong serialized cmd_id");
+}
+
 void testData(const uint8_t *buffer) {
     for (uint8_t i = 0; i < 30; i++) {
         assert(buffer[i] == i && "Packet's data being corrupt during serialization.");
+    }
+}
+
+void testPacket() {
+    assert(packet.cmd == 0x0302 && "Packet's cmd is wrong");
+    for (uint8_t i = 0; i < 30; i++) {
+        assert(packet.data[i] == i && "Packet's data being corrupt during deserialization.");
     }
 }
 
@@ -25,10 +36,11 @@ int main() {
     //Structure Serialize test
     uint8_t buffer[CustomControllerPacketBufferLength];
     assert(serialize(&packet, buffer, CustomControllerPacketBufferLength) == 1 && "Serialize function returned error.");
-    assert(buffer[0] == 0xA5 && buffer[1] == 39 && buffer[2] == 0 && "Wrong serialized frame header.");
-    assert(buffer[5] == 0x02 && buffer[6] == 0x03 && "Wrong serialized cmd_id");
+    testHeader(buffer);
     testData(&buffer[7]);
-    //Structure Deserialize test
+    // Structure Deserialize test
+    deserialize(buffer, &packet);
+    testPacket();
 
     return 0;
 }
