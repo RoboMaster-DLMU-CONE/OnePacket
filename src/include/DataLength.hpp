d266556948c1fc07d5fcc9879e31e3f4c0527450
moonfeather <moonbite233@gmail.com>
2025-03-25 22:38:53 +0800

    add cstdint include

--- src/include/DataLength.hpp
+++ src/include/DataLength.hpp
@@ -1,6 +1,7 @@
 #ifndef DATALENGTH_HPP
 #define DATALENGTH_HPP
 
+#include <cstdint>
 #include <unordered_map>
 using DataLengthMap = std::unordered_map<uint16_t, uint8_t>;
 static DataLengthMap data_length_map = {{0x0101, 1}};
